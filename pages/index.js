44b210c9b5f5122f5c1fb1341c9db998cecae719
Harits <iharitssulaiman@gmail.com>
2025-04-03 03:26:43 +0700

    fix: update contract address

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         "https://tea-sepolia.g.alchemy.com/v2/Wa-bUwSDb2nujeYWyIZ9eHK3XXsxiM8j"
     ];
 
-    const contractAddress = "0x4842A51Fac74B11aAD565134bD9f79e8b6dA5D47";
+    const contractAddress = "0x17fC56B4D518983cFD81b459D04E952D67BFC101";
 
     const abi = [
         "function gm() external",
